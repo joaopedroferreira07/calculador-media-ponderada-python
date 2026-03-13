3178fbb66b74eb0f5e1f051a307e9d7653ad8df9
João Pedro Ferreira <jpedroferreira0701@gmail.com>
2026-03-12 21:22:37 -0300

    Weighted mean calculator

---
+++ main.py
@@ -0,0 +1,10 @@
+# Media ponderada
+p_1 = float(input("Qual foi a sua nota na prova 1?"))
+p_2 = float(input("Qual foi a sua nota na prova 2?"))
+p_3 = float(input("Qual foi a sua nota na prova 3?"))
+
+mediap = (p_1 * 0.4 + p_2 * 0.4 + p_3 * 0.2)
+if mediap >= 7:
+  print("Aprovado")
+else:
+  print("Reprovado")
